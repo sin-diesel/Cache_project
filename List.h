c87eba78cc555329df1af60767dcd0e2bffd803c
Minerkow <melnikov.ignat@phystech.edu>
2020-05-03 15:02:57 +0300

    transition to pointers

--- List.h
+++ List.h
@@ -18,46 +18,46 @@ struct list_t{
 };
 
 //init list of the selected size
-void Init_List(struct list_t list, int size);
+struct list_t* Init_List(int size);
 
 //access the first element
-struct node_t* Front_Elem(struct list_t list);
+struct node_t* Front_Elem(struct list_t* list);
 
 //access the last element
-struct node_t* Back_Elem(struct list_t list);
+struct node_t* Back_Elem(struct list_t* list);
 
 //access to the element by page
-struct node_t* Page_Elem(struct list_t list);
+struct node_t* Page_Elem(struct list_t* list);
 
 //access to the element by hash
-struct node_t* Hash_Elem(struct list_t list);
+struct node_t* Hash_Elem(struct list_t* list);
 
 //check whether the underlying container is empty
-bool Is_Empty (struct list_t list);
+bool Is_Empty (struct list_t* list);
 
 //returns the number of elements
-size_t Size_List (struct list_t list);
+size_t Size_List (struct list_t* list);
 
 //add elem in the end of the list, top of the list deleted
-void Push_Back(struct list_t list, int page, int hash);
+void Push_Back(struct list_t* list, int page, int hash);
 
 //add elem in the top of the list, end of the list deleted
-void Push_Front(struct list_t list, int page , int hash);
+void Push_Front(struct list_t* list, int page , int hash);
 
 //add elem in the top of the list1 an, end of the list moves to the top of the list2
-void Exchange_Elem(struct list_t list1, struct list_t list2, int page, int hash);
+void Exchange_Elem(struct list_t* list1, struct list_t* list2, int page, int hash);
 
 //delete elem in the end
-void Pop_Back (struct list_t list);
+void Pop_Back (struct list_t* list);
 
 //delete elem in the top
-void Pop_Front (struct list_t list);
+void Pop_Front (struct list_t* list);
 
 //delete elem by page
-void Delete_by_Page(struct list_t list, int page);
+void Delete_by_Page(struct list_t* list, int page);
 
 //delete elem by hash
-void Delete_by_Hash(struct list_t list, int hash);
+void Delete_by_Hash(struct list_t* list, int hash);
 
 //free list
-void Free_List (struct list_t list);
+void Free_List (struct list_t* list);
