919de851219662490b252ffab7c4d0e7c25e9a82
Minerkow <melnikov.ignat@phystech.edu>
2020-05-07 23:48:04 +0300

    NOOOOOOO HASHHHH, NOOOOOOOOOO HASH

--- List.c
+++ List.c
@@ -18,7 +18,7 @@ struct list_t* Init_List(int size, struct hash_table* hashTable)
     for (int i = 1; i < size; ++i)
     {
         tmp->next = Create_Node();
-        tmp->next->page = i; tmp->next->hash = i;
+        tmp->next->page = i;
         tmp->next->prev = tmp;
         tmp = tmp->next;
     }
@@ -40,34 +40,6 @@ struct node_t* Back_Elem(struct list_t* list)
     return list->back_elem;
 }
 
-int Hash_with_Page(struct list_t* list, int page)
-{
-    struct node_t* node;
-    assert(list);
-    node = list->front_elem;
-    while (node != NULL)
-    {
-        if (node->page == page)
-            return node->hash;
-        node = node->next;
-    }
-    return -1;
-}
-
-int Page_with_Hash(struct list_t* list, int hash)
-{
-    struct node_t* node;
-    assert(list);
-    node = list->front_elem;
-    while (node != NULL)
-    {
-        if (node->hash == hash)
-            return node->page;
-        node = node->next;
-    }
-    return -1;
-}
-
 char Is_Empty (struct list_t* list)
 {
     assert(list);
@@ -87,7 +59,7 @@ size_t Size_List (struct list_t* list)
     return list->size;
 }
 
-void Push_Back(struct list_t* list, int page, int hash)
+void Push_Back(struct list_t* list, int page)
 {
     if (Is_Empty(list))
     {
@@ -99,7 +71,6 @@ void Push_Back(struct list_t* list, int page, int hash)
     if (list->size == 1)
     {
         list->back_elem->page = page;
-        list->back_elem->hash = hash;
         return;
     }
     old_back = list->back_elem;
@@ -110,7 +81,6 @@ void Push_Back(struct list_t* list, int page, int hash)
     list->front_elem = list->front_elem->next;
     list->front_elem->prev = NULL;
 
-    new_back->hash = hash;
     new_back->page = page;
     new_back->prev = old_back;
     new_back->next = NULL;
@@ -121,7 +91,7 @@ void Push_Back(struct list_t* list, int page, int hash)
     hash_add_elem(page, list->hashTable, list->back_elem);
 }
 
-void Push_Front(struct list_t* list, int page , int hash)
+void Push_Front(struct list_t* list, int page)
 {
     Print_List_Back(list);
     if (Is_Empty(list))
@@ -134,7 +104,6 @@ void Push_Front(struct list_t* list, int page , int hash)
     if (list->size == 1)
     {
         list->back_elem->page = page;
-        list->back_elem->hash = hash;
         return;
     }
     old_front = list->front_elem;
@@ -145,7 +114,6 @@ void Push_Front(struct list_t* list, int page , int hash)
     list->back_elem = list->back_elem->prev;
     list->back_elem->next = NULL;
 
-    new_front->hash = hash;
     new_front->page = page;
     new_front->prev = NULL;
     new_front->next = old_front;
@@ -156,27 +124,26 @@ void Push_Front(struct list_t* list, int page , int hash)
     hash_add_elem(page, list->hashTable, list->front_elem);
 }
 
-void Exchange_Elem(struct list_t* list1, struct list_t* list2, int page, int hash)
+void Exchange_Elem(struct list_t* list1, struct list_t* list2, int page)
 {
     int page_back = list1->back_elem->page;
-    int hash_back = list2->back_elem->hash;
-    Push_Front(list1, page, hash);
-    Push_Front(list2, page_back, hash_back);
+    Push_Front(list1, page);
+    Push_Front(list2, page_back);
 }
 
 void Move_Elem_Page(struct list_t* list, int page)
 {
     assert(list);
-    struct node_t* node = list->front_elem;
+    struct node_t* node = NULL;
     struct node_t* tmp = NULL;
-    while (node->page != page)
-    {
-        node = node->next;
-        if (node == NULL)
-            return;
-    }
+
+    node = hash_page_position(page, list->hashTable);
+    if(node == NULL)
+        return;
+
     if (node == list->front_elem)
         return;
+
     if (node == list->back_elem)
     {
         tmp = node->prev;
@@ -187,35 +154,6 @@ void Move_Elem_Page(struct list_t* list, int page)
             node->prev->next = node->next;
             node->next->prev = node->prev;
         }
-    node->next = list->front_elem;
-    list->front_elem->prev = node;
-    list->front_elem = node;
-    node->prev = NULL;
-}
-
-void Move_Elem_Hash(struct list_t* list, int hash)
-{
-    assert(list);
-    struct node_t* node = list->front_elem;
-    struct node_t* tmp = NULL;
-    while (node->hash != hash)
-    {
-        node = node->next;
-        if (node == NULL)
-            return;
-    }
-    if (node == list->front_elem)
-        return;
-    if (node == list->back_elem)
-    {
-        tmp = node->prev;
-        tmp->next = NULL;
-        list->back_elem = tmp;
-    } else
-    {
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
-    }
     node->next = list->front_elem;
     list->front_elem->prev = node;
     list->front_elem = node;
@@ -243,7 +181,7 @@ void Print_List_Front(struct list_t* list)
     struct node_t* tmp = list->front_elem;
     while (tmp != NULL)
     {
-        printf("[p-%d, h-%d]->", tmp->page, tmp->hash);
+        printf("[%d]->", tmp->page);
         tmp = tmp->next;
     }
     printf("\n");
@@ -256,7 +194,7 @@ void Print_List_Back(struct list_t* list)
     struct node_t* tmp = list->back_elem;
     while (tmp != NULL)
     {
-        printf("<-[p-%d, h-%d]", tmp->page, tmp->hash);
+        printf("<-[%d]", tmp->page);
         tmp = tmp->prev;
     }
     printf("\n");
@@ -268,3 +206,25 @@ static struct node_t* Create_Node()
     assert(node);
     return node;
 }
+
+/*
+void Assert_List(struct list_t* list)
+{
+    Is_Empty(list);
+    if(list->front_elem->prev == NULL)
+    {
+        fprintf(stderr, "ERROR: There is pointer to the parent of the front node");
+        exit(3);
+    }
+    if(list->back_elem->next == NULL)
+    {
+        fprintf(stderr, "ERROR: There is pointer to the child of the back node");
+        exit(3);
+    }
+    struct node_t* node = list->front_elem;
+    while (node != NULL)
+    {
+
+    }
+}
+*/
