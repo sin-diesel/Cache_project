c54386cb755fb30fec36520f38262c1831a1bc17
Minerkow <melnikov.ignat@phystech.edu>
2020-05-18 10:23:46 +0300

    add comments

--- include/List.h
+++ include/List.h
@@ -21,55 +21,59 @@ struct list_t{
     int size;
 };
 
-//access the first element
+// access the first element
 static inline int Front_Elem(struct list_t* list){return list->front_elem->page;}
 
-//access the last element
+// access the last element
 static inline int Back_Elem(struct list_t* list){return list->back_elem->page;}
 
-//returns the number of elements
+// returns the number of elements
 static inline int Size_List (struct list_t* list){return list->size;};
 
-//init list of the selected size
+// init list of the selected size
 struct list_t* Init_List(int size, struct hash_table* hashTable);
 
+// Create node for list
 struct node_t* Create_Node();
 
-//check whether the underlying container is empty
+// check whether the underlying container is empty
 char Is_Empty (struct list_t* list);
 
-//add elem in the end of the list, top of the list deleted
+// add elem in the end of the list, top of the list deleted
 void Push_Back(struct list_t* list, int page);
 
-//add elem in the top of the list, end of the list deleted
+// add elem in the top of the list, end of the list deleted
 void Push_Front(struct list_t* list, int page);
 
-//add elem in the top of the list1 an, end of the list moves to the top of the list2
+// add elem in the top of the list1 an, end of the list moves to the top of the list2
 void Exchange_Elem(struct list_t* list1, struct list_t* list2, int page);
 
-//Move elem forward by page
-//if page not found nothing happens
+// Move elem forward by page
 void Move_Elem_Page(struct list_t* list, int page);
 
-//Send to main , delete in out
+// Send to main , delete in out
 void Send_to_Main(struct list_t* out, struct list_t* main, int page);
 
-//free list
+// free list
 void Free_List (struct list_t* list);
 
-//print list from the front
+// print list from the front
 void Print_List_Front(struct list_t* list);
 
-//print list from the back
+// print list from the back
 void Print_List_Back(struct list_t* list);
 
-//Assert List
+// Assert List
 void Assert_List(struct list_t* list);
 
+// Create array from list
 int* Create_ArrayByList(struct list_t* list);
 
+// Create list from array
 struct list_t* Create_ListByArray(int* arr, int len);
 
+// Resize List
 void Resize_List(struct list_t* list, int newsize);
 
+// Fill list
 void Fill_List(struct list_t* list ,int val);
